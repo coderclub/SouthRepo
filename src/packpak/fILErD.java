eddbf97f8ce6f945c8c0b26c35d39354a9a8103c
mnihussain <md.nasiribnehussain@gmail.com>
2018-09-02 15:12:24 -0400

    hi south guys here is your rep

---
+++ src/packpak/fILErD.java
@@ -0,0 +1,36 @@
+package packpak;
+
+import java.io.FileReader;
+import java.util.Scanner;
+import java.*;
+import java.io.BufferedReader;
+
+public class fILErD {
+
+	public static void main(String[] args) {
+	int a ;
+		FileReader fr =null;
+		BufferedReader br = null;
+		String path = "C:\\Users\\mdnas\\Desktop\\PuBg.txt";
+
+		try {
+			fr = new FileReader(path);
+		} catch (Exception ex) {
+			System.out.println("FILE NOT LOCATED");
+		}
+
+		try {
+			br = new BufferedReader(fr);
+			String data = "";
+			while ((data = br.readLine()) != null) {
+				System.out.println(data);
+			
+		}
+				;
+		} catch (Exception ex) {
+			System.out.println("File is not readble");
+		}
+
+	}
+
+}
